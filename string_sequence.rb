6c50444d0a0a8bc6c2c8b896c0faaa7c127f9510
latticetower <merlettaia@gmail.com>
2013-11-19 18:34:20 +0400

    sequence class now can accumulate strings

--- string_sequence.rb
+++ string_sequence.rb
@@ -5,10 +5,32 @@ ruby 1.9.3 compatible
 =end
 class StringSequence < Array
   def initialize(*args)
-    super(args)
+    super
   end
   def from_string(str)
-    self = str.to_a.map{|chr| [chr] }
+    str.chars.map{|chr| [chr] }
+  end
+  def << (str)
+    if self.size > str.size
+      self.zip(str.chars.map{|chr| [chr] } ).map{|x| x.inject(&:+)}
+    else
+      a = str.chars.map{|chr| [chr] }.zip(self).map{|x| x.compact.inject(&:+)}
+      self.clear
+      a.each {|x| self.push(x) }
+    end
+    self
+  end
+  
+  def to_s()
+    self.map{|x| "[" + x.sort{|x, y| x.ord <=> y.ord}.join(', ') + "]"}.join(', ')
   end
   
 end
+
+#
+# usage example
+#
+ss = StringSequence.new
+ss << "AAAAAAAA" << "BBBBBBBBBB"
+
+puts ss.to_s
