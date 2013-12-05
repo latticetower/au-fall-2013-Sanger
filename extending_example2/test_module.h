bb219fcb91d417aeb6483b8fca8d11337b9eea5c
latticetower <merlettaia@gmail.com>
2013-12-05 13:33:30 +0400

    Тесты проходят

--- extending_example2/test_module.h
+++ extending_example2/test_module.h
@@ -5,7 +5,7 @@ public:
 private:  
   type_t m_t;  
 public:  
-  MyTest() {}  
+  MyTest(): m_t(0) {}  
   MyTest(const type_t& t) {m_t = t;}  
   void set(const type_t& val);  
   const type_t& get() const;  
--- extending_example2/test_module_spec.rb
+++ extending_example2/test_module_spec.rb
@@ -3,12 +3,13 @@ require './test_module'
 
 describe 'test_module' do 
   it 'should do something' do 
-    t1 = ::MyTest.new  
+  #puts Test_module.methods
+    t1 = Test_module::MyTest.new  
     t1.get.must_equal 0.0 #-> 0.0  
     t1.set(12.34)  
     t1.get.must_equal 12.34  
       
-    t2 = ::MyTest.new(45.67)  
+    t2 = Test_module::MyTest.new(45.67)  
     t2.get.must_equal 45.67  
   end
 end
